791ba982a20b42617bcf8fbffb9ee498143fdb7c
nsmith7989 <nathanael@gorocketfuel.com>
2015-08-31 20:18:26 -0500

    correct signature of filter functions

--- src/selectors/filter.js
+++ src/selectors/filter.js
@@ -3,27 +3,25 @@ import { createSelector } from 'reselect';
 import keywordFilter from '../helpers/keywordFilter.js'
 
 
-function filter(appliedFilters, collection) {
+function filter(appliedFilters, collection, functions) {
 
     return collection.filter(item => {
 
         if (!appliedFilters.size) return true;
 
         // filters can either be AND or OR between groups, change filterFn in config.js (always AND within group)
-        // todo: signature of these filters is wrong
-        return filterFn((attribute, map) => {
-            return some((key) => {
-                const fn = attributeFilterFn[attribute + '__' + key];
-                return (typeof fn === 'function') ? fn(item) : false;
-            }, map);
-        }, appliedFilters);
-
+        return item[filterFn](attribute => {
+           return attribute.some(key => {
+               const fn = functions[attribute + '__' + key];
+               return (typeof fn === 'function') ? fn(item) : false;
+           });
+        });
     });
 
 }
 
-const composedSelector = (appliedFilters, keyword) => {
-    const filteredResults = filter(appliedFilters, collection);
+const composedSelector = (appliedFilters, keyword, functions) => {
+    const filteredResults = filter(appliedFilters, collection, functions);
     const keywordFiltered = keywordFilter(filteredResults, keyword);
     // other filters or sorts go here
     return keywordFiltered;
@@ -31,8 +29,9 @@ const composedSelector = (appliedFilters, keyword) => {
 
 const appliedFiltersSelector = state => state.appliedFilters;
 const keywordSelector = state => state.searchKeyword;
+const functions = state => state.filterFns;
 
 export default createSelector(
-    [appliedFiltersSelector, keywordSelector],
+    [appliedFiltersSelector, keywordSelector, functions],
     composedSelector
 );

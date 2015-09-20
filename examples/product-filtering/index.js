896e14f5604179be1fb28e59565508b0e81ec65f
nsmith7989 <nathanael@gorocketfuel.com>
2015-09-20 10:05:12 -0500

    add example setup

---
+++ examples/product-filtering/index.js
@@ -0,0 +1,11 @@
+import React, { Component, render } from 'react';
+
+class App extends Component {
+
+    render() {
+        return <h1>Hello World!!</h1>
+    }
+
+}
+
+render(<App />, document.getElementById('root'));

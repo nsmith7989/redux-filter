9c9c595c16430575db24d5bfaa2947cdfa729769
nsmith7989 <nathanael.b.smith@gmail.com>
2015-11-28 20:32:18 -0600

    refactor toggles

--- src/helpers/toggle.js
+++ src/helpers/toggle.js
@@ -1,3 +1,4 @@
+import omit from 'lodash.omit';
 /**
  * {
  * [attribute]: [values],
@@ -6,39 +7,45 @@
  *
  */
 
+function removeElement(obj, key, arr, index) {
+    const removedArray = [
+        ...arr.slice(0, index),
+        ...arr.slice(index + 1)
+    ];
+    // if there is no items on the array, do not set a key on the filter object
+    if (removedArray.length) {
+        return {
+            ...obj,
+            [key]: removedArray
+        };
+    } else {
+        // no item here, remove that key (in a non mutative way)
+        return omit(obj, key);
+    }
+}
+
+function addElement(obj, key, arr, value) {
+    return {
+        ...obj,
+        [key]: arr.concat(value)
+    };
+}
+
 export default function toggle(filters, key, value) {
-
-    //copy
-    filters = {...filters};
-
-    let currentFilterArray = (filters[key] && filters[key].slice(0)) || [];
-    // remove the reference to the current subject because we're mutating it
-    delete filters[key];
-
+    let currentFilterArray = filters[key] || [];
     // check if the value is in the currentFilterArray, then add or remove it
     const indexOfValueInCurrentFilterArray = currentFilterArray.indexOf(value);
     if (indexOfValueInCurrentFilterArray > -1) {
-        // remove value form array
-        currentFilterArray.splice(indexOfValueInCurrentFilterArray, 1);
+        return removeElement(filters, key, currentFilterArray, indexOfValueInCurrentFilterArray);
     } else {
-        // add value to array
-        currentFilterArray.push(value);
+        return addElement(filters, key, currentFilterArray, value);
     }
-    if (!currentFilterArray.length) {
-        return {...filters};
-    } else {
-        return {...filters, [key]: currentFilterArray};
-    }
-
 }
 
 export function toggleOnly(filters, key, value) {
-    filters = {...filters};
-    return {...filters, [key]: [value]};
+    return { ...filters, [key]: [value] };
 }
 
 export function clearFilter(filters, key) {
-    filters = {...filters};
-    delete filters[key];
-    return {...filters};
+    return omit(filters, key);
 }
